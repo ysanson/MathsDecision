8e94fbbdf6ab2d03efd27c703ce8e16e7cb6bc60
Yvan Sanson <yvan.sanson@etu.umontpellier.fr>
2019-02-17 11:43:14 +0100

    Modifs mineures

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-import rangs, groups, fileOperations
+import ranks, groups, fileOperations
 
 
 #This function returns a matrix containing the student's appreciation for each other.
 def createStudentsAppreciations():
+    #fileOperations.readAppreciationsCSV()
     students = [[-1,"B","TB","TB","B"],
                 ["TB", -1, "B", "B", "P"],
                 ["AB", "B", -1, "AB", "B"],
@@ -18,9 +19,15 @@ def printMatrix(matrix):
 
 students = createStudentsAppreciations()
 n = len(students)
-ME = rangs.attributeRanks(students)
+ME = ranks.attributeRanks(students)
 printMatrix(ME)
-NR = rangs.countRanks(ME, n)
+NR = ranks.countRanks(ME, n)
+nbBinomes, nbTrinomes=0,0
+if n/2>18:
+    nbTrinomes = n-36
+    nbBinomes = 18-nbTrinomes
+else:
+    nbBinomes, nbTrinomes = n/2, 0 
 printMatrix(NR)
 groupsOfTwo, studentsLeft = groups.createGroupsOfTwo(ME, n)
 print("groups of 2")
